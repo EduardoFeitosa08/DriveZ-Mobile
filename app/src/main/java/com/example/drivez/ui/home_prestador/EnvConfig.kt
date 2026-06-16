13b7aadbfbb9748d21be4ff22f5a6bcd5d5c018e
Eduardo Feitosa <eduardo.edufeitosa16@gmail.com>
2026-06-16 19:20:28 -0300

    Chaves de Api Corrigidas

--- app/src/main/java/com/example/drivez/ui/home_prestador/EnvConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.drivez.ui.home_prestador
-
-object EnvConfig {
-    const val SUPABASE_URL = "https://cbhxjdletoafyycmtbhf.supabase.co"
-    const val SUPABASE_KEY = "sb_publishable_VRBtgVQ4-TY-59ImgD9wPQ_k7Qw6Eaj"
-    const val MAPBOX_ACCESS_TOKEN="pk.eyJ1IjoiZWR1YXJkb2ZlaXRvc2EwOCIsImEiOiJjbXFhNWNtZmUwMGd2MnNxNWV2M3FsOGFuIn0.dAq4ynSLSUWH2fik1rvvVw"
-    const val MAPBOX_DOWNLOAD_TOKEN="sk.eyJ1IjoiZWR1YXJkb2ZlaXRvc2EwOCIsImEiOiJjbXBkY3V5OWUwYmduMnBvZ25vaWNncjJ6In0.3scJHfs-MO9EhtG1iq5XZA"
-}
